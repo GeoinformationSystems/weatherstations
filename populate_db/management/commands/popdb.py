47f4b9484bf52fbd9417828e9a2d92d0cb761158
MenschMarcus <mk89@posteo.de>
2017-02-13 15:58:36 +0100

    set character numbers for parsing

--- populate_db/management/commands/popdb.py
+++ populate_db/management/commands/popdb.py
@@ -12,16 +12,94 @@
 
 INPUT_FILES = \
 {
-    'country_codes':    'data/meta/country_codes',
+    'country_codes':
+    {
+        'path':       'data/meta/country_codes',
+        'characters':
+        {
+            'code':     [0,  2],
+            'country':  [4, 60],
+            'END':      60
+        }
+    }
     'temperature' :
     {
-        'stations':     'data/GHCN_monthly_v3_temperature/ghcnm.tavg.v3.3.0.20170203.qca.inv',
-        'data':         'data/GHCN_monthly_v3_temperature/ghcnm.tavg.v3.3.0.20170203.qca.dat'
+        'stations':
+        {
+            'path': 'data/GHCN_monthly_v3_temperature/ghcnm.tavg.v3.3.0.20170203.qca.inv',
+            'characters':
+            {
+                'station_id':   [0,  10],
+                'lat':          [12, 19],
+                'lng':          [21, 29],
+                'elv':          [31, 37],
+                'name':         [38, 68],
+                'country':      [58, 68],   # N.B. is inside name !!!
+                'END':          107,
+            }
+
+        }
+        'data':
+         {
+            'path': 'data/GHCN_monthly_v3_temperature/ghcnm.tavg.v3.3.0.20170203.qca.dat'
+            'characters':
+            {
+                'station_id':   [0,  10],
+                'year':         [11, 14],
+                'value':        [15, 18],   # should always be 'TAVG'
+                'm01':          [19, 23],
+                'm02':          [27, 31],
+                'm03':          [35, 39],
+                'm04':          [43, 47],
+                'm05':          [51, 55],
+                'm06':          [59, 63],
+                'm07':          [67, 71],
+                'm08':          [75, 79],
+                'm09':          [83, 87],
+                'm10':          [91, 95],
+                'm11':          [99, 103],
+                'm12':          [107, 111],
+            }
+         }
     },
     'precipitation' :
     {
-        'stations':     'data/GHCN_monthly_v2_precipitation/v2.prcp.inv',
-        'data':         'data/GHCN_monthly_v2_precipitation/v2.prcp'
+        'stations':
+        {
+            'path': 'data/GHCN_monthly_v2_precipitation/v2.prcp.inv',
+            'characters':
+            {
+                'station_id':   [0,  10],
+                'lat':          [44, 48],
+                'lng':          [50, 57],
+                'elv':          [58, 61],
+                'name':         [12, 42],
+                'country':      [32, 42],   # N.B. is inside name !!!
+                'END':          107,
+            }
+        }
+        'data':
+        {
+            'path': 'data/GHCN_monthly_v2_precipitation/v2.prcp',
+            'characters':
+            {
+                'station_id':       [0,  10],
+                'year':             [12, 15],
+                'has_duplicates':   [11, 11],
+                'm01':              [16, 20],
+                'm02':              [21, 25],
+                'm03':              [26, 30],
+                'm04':              [31, 35],
+                'm05':              [36, 40],
+                'm06':              [41, 45],
+                'm07':              [46, 50],
+                'm08':              [51, 55],
+                'm09':              [56, 60],
+                'm10':              [61, 65],
+                'm11':              [66, 70],
+                'm12':              [71, 75],
+            }
+        }
     }
 }
 
@@ -50,7 +128,6 @@ from populate_db.models import *
 def get_file(file_id):
     return os.path.abspath(
         os.path.join(
-            os.path.dirname(populate_db.__file__),
             file_id
     )
 )
@@ -71,11 +148,22 @@ class Command(NoArgsCommand):
         TemperatureData.objects.all().delete()
         PrecipitationData.objects.all().delete()
 
-        teststation = Station(
-            name = "Horst",
-            country = "Horstland",
-            lat = 0,
-            lng = 0,
-            elev = 0
-        )
-        teststation.save()
+        # preparation: read the country codes
+        country_codes_file = open(get_file(INPUT_FILES.country_codes.path))
+        print(country_codes_file)
+
+        # TEMPERATURE
+
+
+        # PRECIPITATION
+
+
+        # example code: how to populate the database
+        # teststation = Station(
+        #     name = "Horst",
+        #     country = "Horstland",
+        #     lat = 0,
+        #     lng = 0,
+        #     elev = 0
+        # )
+        # teststation.save()
