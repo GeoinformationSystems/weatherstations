1ecce807788ba939979a518e1ffe6ee58871b205
MenschMarcus <mk89@posteo.de>
2017-02-20 14:37:02 +0100

    much more efficient way of counting gaps

--- populate_db/management/commands/updb.py
+++ populate_db/management/commands/updb.py
@@ -11,11 +11,11 @@
 ################################################################################
 
 # check only limited set of data
-TEST_RUN = True
+TEST_RUN = False
 
 # size of consecutive database queries that are executed at once
 # optimal: [1000 .. 5000]
-BULK_SIZE = 50
+BULK_SIZE = 1000
 
 # number of months in a year
 NUM_MONTHS = 12
@@ -83,22 +83,19 @@ class Command(BaseCommand):
         # for each Station
         for station in Station.objects.all():
 
-            print ''
-            print station
-
             # find all connected station data
             # is properly ordered by 1. year 2. station (ascending)
-            station_data = StationData.objects.filter(station=station)
+            station_datas = StationData.objects.filter(station=station)
 
             # find min and max year
-            min_year = station_data.aggregate(Min('year'))['year__min']
-            max_year = station_data.aggregate(Max('year'))['year__max']
+            min_year = station_datas.aggregate(Min('year'))['year__min']
+            max_year = station_datas.aggregate(Max('year'))['year__max']
 
             # get total number of months that lack either temp or prcp data
-            missing_months = station_data.filter(is_complete=False)
+            missing_months = station_datas.filter(is_complete=False)
 
             # get percentage of complete months
-            num_months = station_data.count()
+            num_months = station_datas.count()
             num_missing_months = missing_months.count()
             complete_data_rate = float(num_months-num_missing_months) / float(num_months)
 
@@ -107,78 +104,47 @@ class Command(BaseCommand):
             largest_gap = 0             # largest gap found so far
             currently_in_gap = False    # currently in a gap
             curr_gap_size = 0           # size of current gap
-            station_data_it = 0         # iterator in station data array
-            num_station_data = station_data.count()     # number of data points
 
-            t1 = []
-            t2 = []
+            ## create consecutive list of monthly data: is data complete?
+            ## structure: [min_year/Jan, min_year/Feb, ..., max_yar/Dec]
+            monthly_completion_list = [0]*((max_year-min_year+1)*NUM_MONTHS)
 
-            ## for each year
-            year = min_year
-            while year <= max_year:
+            ## for each station data point
+            for station_data in station_datas:
 
-                ## for each month
-                month = 1
-                while month <= NUM_MONTHS:
+                # get index for monthly_completion_list
+                idx = ((station_data.year - min_year)*NUM_MONTHS) + station_data.month-1
 
-                    t=time.time()
-                    ## is it a gap? (default: yes)
-                    is_gap = True
+                # add information to list: is data point complete?
+                monthly_completion_list[idx] = station_data.is_complete
 
-                    ## check if it is NOT a gap, i.e. check if data is available
-                    ## 1) iterator / index is in range
-                    if  (station_data_it <= num_station_data):
-                        this_station_data = station_data[station_data_it]
+            ## for each monthly completion data value (= is it a gap?)
+            for month_complete in monthly_completion_list:
 
-                        ## 2) this particular combination year+month exists
-                        if  (this_station_data.year is year) and \
-                            (this_station_data.month is month):
+                ## if it is a gap, count the gap size. 4 cases:
+                if currently_in_gap:
 
-                            ## 3) station data is complete => no gap
-                            if (this_station_data.is_complete):
-                                is_gap = False
+                    ## 1) currently in a gap and gap continues => increment!
+                    if not month_complete:
+                        gap += 1
 
-                            ## => iterate to next station data point
-                            station_data_it += 1
-
-                    ## in other cases: data either not available or incomplete
-                    ## => it is a gap
-
-                    t1.append(time.time()-t)
-                    t=time.time()
-
-                    ## if it is a gap, count the gap size. 4 cases:
-                    if currently_in_gap:
-
-                        ## 1) currently in a gap and gap continues => increment!
-                        if is_gap:
-                            gap += 1
-
-                        ## 2) currently in a gap and gap ended => finalize!
-                        ## -> gap size n has one more occurence
-                        else:
-                            gaps[gap] += 1
-                            largest_gap = max(largest_gap, gap)
-                            gap = 0
-                            currently_in_gap = False
-
+                    ## 2) currently in a gap and gap ended => finalize!
+                    ## -> gap size n has one more occurence
                     else:
+                        gaps[gap] += 1
+                        largest_gap = max(largest_gap, gap)
+                        gap = 0
+                        currently_in_gap = False
 
-                        ## 3) currently not in a gap and gap found => start gap
-                        if is_gap:
-                            gap = 1
-                            currently_in_gap = True
+                else:
 
-                        ## 4) currently not in a gap and no gap found => continue!
-                        else: pass
+                    ## 3) currently not in a gap and gap found => start gap
+                    if not month_complete:
+                        gap = 1
+                        currently_in_gap = True
 
-                    t2.append(time.time()-t)
-
-                    ## next month
-                    month += 1
-
-                ## next year
-                year += 1
+                    ## 4) currently not in a gap and no gap found => continue!
+                    else: pass
 
             # finalize last gap
             if currently_in_gap:
@@ -187,11 +153,8 @@ class Command(BaseCommand):
                 gap = 0
                 currently_in_gap = False
 
-            print "t1", sum(t1)
-            print "t2", sum(t2)
 
             # finally write the data
-            print min_year, max_year, num_missing_months, complete_data_rate, largest_gap
             station.min_year =              min_year
             station.max_year =              max_year
             station.missing_months =        num_missing_months
